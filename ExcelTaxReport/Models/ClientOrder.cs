06b2f1dc06179fe26b81549e71af76dca49a4744
erice065 <r.eldis@yahoo.com>
2018-05-18 18:10:56 -0400

    ParcelInformation w/ properties. Added to ClientOrder.

--- ExcelTaxReport/Models/ClientOrder.cs
+++ ExcelTaxReport/Models/ClientOrder.cs
@@ -7,11 +7,13 @@ namespace ReportBuilder.Models
     {
         public ReportConfig report_config { get; set; }
         public ClientConfig client_config { get; set; }
+        public List<ParcelInformation> Parcels { get; set; }
 
         public ClientOrder()
         {
             this.report_config = new ReportConfig();
             this.client_config = new ClientConfig();
+            this.Parcels = new List<ParcelInformation>();
         }
     }
 }
