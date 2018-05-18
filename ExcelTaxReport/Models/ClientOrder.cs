24763e3a554ade189e11c70a4ee78f0ab840e37e
erice065 <r.eldis@yahoo.com>
2018-05-18 17:46:30 -0400

    ReportConfig w/ disclaimer. Added to ClientOrder

--- ExcelTaxReport/Models/ClientOrder.cs
+++ ExcelTaxReport/Models/ClientOrder.cs
@@ -5,6 +5,11 @@ namespace ReportBuilder.Models
 {
     public class ClientOrder
     {
-        
+        public ReportConfig report_config { get; set; }
+
+        public ClientOrder()
+        {
+            this.report_config = new ReportConfig();
+        }
     }
 }
