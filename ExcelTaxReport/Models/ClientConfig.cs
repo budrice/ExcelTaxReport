9695f842764b7ab1843f60c767c69b8a7001a3aa
erice065 <r.eldis@yahoo.com>
2018-05-18 17:54:44 -0400

    ClientConfig w/ properties. Added to ClientOrder.

---
+++ ExcelTaxReport/Models/ClientConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace ReportBuilder.Models
+{
+    public class ClientConfig
+    {
+        public int id { get; set; }
+        public int client_id { get; set; }
+        public string report_name { get; set; }
+        public string base_path { get; set; }
+        public string logo { get; set; }
+        //public string date_due_field { get; set; }
+        //public byte detailed_additional_info { get; set; }
+    }
+}
--- ExcelTaxReport/Models/ClientOrder.cs
+++ ExcelTaxReport/Models/ClientOrder.cs
@@ -6,10 +6,12 @@ namespace ReportBuilder.Models
     public class ClientOrder
     {
         public ReportConfig report_config { get; set; }
+        public ClientConfig client_config { get; set; }
 
         public ClientOrder()
         {
             this.report_config = new ReportConfig();
+            this.client_config = new ClientConfig();
         }
     }
 }
