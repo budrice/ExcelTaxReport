449b4277ccae1990d2e2fccbba65f86e0a906146
erice065 <r.eldis@yahoo.com>
2018-05-19 17:36:12 -0400

    Report1 start

---
+++ ExcelTaxReport/Reports/Report1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelTaxReport;
+using ExcelTaxReport.Models;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace ExcelTaxReport.Reports
+{
+    class Report1 : IReport
+    {
+        public Report1(ClientOrder client_order)
+        {
+            
+        }
+
+        public bool CreateReport()
+        {
+            return false;
+        }
+
+    }
+}
