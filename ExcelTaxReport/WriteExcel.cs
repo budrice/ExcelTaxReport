9815ccbb3bf7eed91e563fe230d3d69036f1da37
erice065 <r.eldis@yahoo.com>
2018-05-19 23:31:16 -0400

    ColorMergeRow and CellValue functions

---
+++ ExcelTaxReport/WriteExcel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelTaxReport.Models;
+
+namespace ExcelTaxReport
+{
+    public class WriteExcel
+    {
+        public ClientOrder client_order;
+
+        public Excel.Application xlApp;
+        public Excel.Workbook xlWorkbook;
+        public Excel.Workbooks xlWorkbooks;
+        public Excel.Worksheet sheet1;
+        public Excel.Worksheet sheet2;
+        public Excel.Worksheet sheet3;
+
+        public void ColorMergedRow(int row, decimal height = 15M, string start_col = "A", string end_col = "K", Color? color = null, bool border = true)
+        {
+            Color rgb = color ?? Color.FromArgb(192, 192, 192);
+            sheet1.Range[start_col + row, end_col + row].Merge();
+            if (border == true)
+            {
+                sheet1.Range[start_col + row, end_col + row].Cells.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            }
+            sheet1.Cells[row, start_col].Interior.Color = ColorTranslator.ToOle(rgb);
+            sheet1.Cells[row, start_col].RowHeight = height;
+        }
+
+        public void CellValue(string cell, string value, int fontsize = 10, Excel.XlHAlign halign = Excel.XlHAlign.xlHAlignLeft, Excel.XlVAlign valign = Excel.XlVAlign.xlVAlignBottom, Color? font_color = null, string font = "Arial", bool bold = true, bool underline = false)
+        {
+            int row;
+            Int32.TryParse(new String(cell.Where(Char.IsDigit).ToArray()), out row);
+            string col = new String(cell.Where(Char.IsLetter).ToArray());
+            Color font_rgb = font_color ?? Color.FromArgb(0, 0, 0);
+
+            sheet1.Cells[row, col].WrapText = true;
+            sheet1.Cells[row, col].Font.Name = font;
+            sheet1.Cells[row, col].Font.Size = fontsize;
+            sheet1.Cells[row, col].Font.Bold = bold;
+            sheet1.Cells[row, col].Font.Underline = underline;
+            sheet1.Cells[row, col].Font.Color = ColorTranslator.ToOle(font_rgb);
+            sheet1.Cells[row, col] = value;
+            sheet1.Cells[row, col].HorizontalAlignment = halign;
+            sheet1.Cells[row, col].VerticalAlignment = valign;
+        }
+
+    }
+}
